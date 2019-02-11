4c91fa81ebcaf0d4ea54b532fc93cd2e3a92a91b
Michael Bilan <mabilan@alaska.edu>
2019-02-11 14:36:48 -0900

    Initial commit  for assignment 2

---
+++ Assignment 2/pa2.lua
@@ -0,0 +1,22 @@
+-- Michael Bilan
+-- CS 331 Homework Assignment 2
+-- Created: 8 Feb 2019
+-- Updated: 8 Feb 2019
+-- This file is a Lua module containing 4 module functions:
+--		mapTable, concatMax, collatz, and backSubs
+
+local pa2 = {}
+
+	local function mapTable(f, t)
+	end
+	
+	local function concatMax(s, t)
+	end
+
+	local function collatz(k)
+	end
+	
+	local function backSubs(s)
+	end
+	
+return pa2
