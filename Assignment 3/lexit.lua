4ae53aeae4df68248c0c98967a6e22ad8ff2e8a8
Michael Bilan <mabilan@alaska.edu>
2019-02-20 22:53:49 -0900

    Lexer finished

--- Assignment 3/lexit.lua
+++ Assignment 3/lexit.lua
@@ -1,29 +1,30 @@
+-- lexit.lua
 -- Michael Bilan
--- CS 331
--- Assignment 3 - Lexer in Lua
--- Created: 2/19/2019
--- Last Modified: 2/20/2019
--- This code is a Lua module that performs lexical anaylsis according the the lexeme specification provided for Assignment 3
--- Lexeme spec is for the 'Jerboa' language
+-- Started: 2019/02/19
+-- Last Modified: 2019/02/20
+-- Based on code from:
+-- 		lexit.lua
+--		Glenn G. Chappell
+-- *********************************************************************
+-- Module Table Initialization
+-- *********************************************************************
+local lexit = {}  
 
-
--- initialize module table
-
-local lexit = {}
-
-	-- ********************************************************************
-	-- Public Constants
-	-- Numeric constants representing lexeme classes, array of lexeme names
-	-- ********************************************************************
-
-	lexit.KEY = 1
-	lexit.ID = 2
+-- *********************************************************************
+-- Public Constants
+-- *********************************************************************
+	-- Numeric constants representing lexeme categories
+	lexit.KEY    = 1
+	lexit.ID     = 2
 	lexit.NUMLIT = 3
 	lexit.STRLIT = 4
-	lexit.OP = 5
-	lexit.PUNCT = 6
-	lexit.MAL = 7
-	
+	lexit.OP     = 5
+	lexit.PUNCT  = 6
+	lexit.MAL    = 7
+
+	-- catnames
+	-- Array of names of lexeme categories.
+	-- Human-readable strings. Indices are above numeric constants.
 	lexit.catnames = {
 		"Keyword",
 		"Identifier",
@@ -33,14 +34,9 @@ local lexit = {}
 		"Punctuation",
 		"Malformed"
 	}
-	
-	
-	-- *******************************************************
-	-- Local Constants
-	-- Tables of keywords and operators for validation testing
-	-- *******************************************************
-	
-	-- Table of keywords for this lexeme spec, stored for scalability
+
+	-- keywords
+	-- Local table containing all lexeme keywords
 	local keywords = {
 		"cr",
 		"def",
@@ -56,34 +52,11 @@ local lexit = {}
 		"write",
 	}
 
-	
-	-- Table of operators for this lexeme spec, stored for scalability
-	local operators = {
-		"&&",
-		"||",
-		"!",
-		"==",
-		"!=",
-		"<",
-		"<=",
-		">",
-		">=",
-		"+",
-		"-",
-		"*",
-		"/",
-		"%",
-		"[",
-		"]",
-		"=",
-	}
-
-	
-	-- *******************************************************
-	-- Character-checking functions
-	-- Functions return -1 if passed a string with length != 1
-	-- *******************************************************
-	
+-- *********************************************************************
+-- Kind-of-Character Functions
+-- *********************************************************************
+	-- All functions return false when given a string whose length is not
+	-- exactly 1.
 	-- isLetter
 	-- Returns true if string char is a letter character, false otherwise.
 	local function isLetter(char)
@@ -107,27 +80,6 @@ local lexit = {}
 		else
 			return false
 		end
-	end
-	-- isQuote
-	-- Returns true is the string  char is a single or double quote
-	local function isQuote(char)
-		if char:len() ~= 1 then
-			return false
-		elseif char == "\"" or char == "\'" then
-			return true
-		else
-			return false
-		end
-	end
-	-- isOperatorInitiator
-	-- Returns true if string char could start an operator, false otherwise.
-	local function isOperator(char)
-		for index = 1, operators do
-			if operators[index] == char then
-				return true
-			end
-		end
-		return false
 	end
 	-- isWhitespace
 	-- Returns true if string char is a whitespace character, false otherwise.
@@ -155,154 +107,176 @@ local lexit = {}
 		end
 	end
 	-- isKeyword
-	-- Returns true if lexeme is a keyword/reserved word, false otherwise.
+	-- Returns true if the string lexeme matches a keyword from the lexeme
+	-- specification, false otherwise.
 	local function isKeyword(lexeme)
-		for index = 1, keywords do
-			if keywords[index] == lexeme then
+		for index, value in ipairs(keywords) do
+			if lexeme == value then
+				return true
+			end
+		end
+		return false
+	end
+	-- isOperator
+	-- Returns true if the string lexeme matches an operator from the lexeme
+	-- specification, false otherwise.
+	local function isOperator(lexeme)
+		for index, value in ipairs(operators) do
+			if lexeme == value then
 				return true
 			end
 		end
 		return false
 	end
 	
-
-	
-	-- ***************************************************************************
+	-- *********************************************************************
+	-- The Lexer
+	-- *********************************************************************
 	-- lex
-	-- Lexing function, intended for use in a for-in loop:
-	--		for lexString, category in lexer.lex(program) do
-	-- Here, lexString is the string form of a lexeme, and category is the numeric
-	-- representation of the lexeme category from the public constants above.
-	-- ***************************************************************************
-	
+	-- Our lexer
+	-- Intended for use in a for-in loop:
+	--     for lexString, cat in lexit.lex(program) do
+	-- Here, lexString is the string form of a lexeme, and cat is a number
+	-- representing a lexeme category. (See Public Constants.)
 	function lexit.lex(program)
-		-- Local 'data members'
+		-- ***** Variables (like class data members) *****
+		local position  -- Index of next character in program
+						-- INVARIANT: when getLexeme is called, position is
+						--  EITHER the index of the first character of the
+						--  next lexeme OR program:len()+1
+		local state     -- Current state for our state machine
+		local char      -- Current character
+		local lexString  -- The lexeme, so far
+		local category  -- Category of lexeme, set when state set to DONE
+		local handlers  -- Dispatch table; value created later
+		local previousLexeme	-- copy of the most recent lexeme, if one exists
+		local previousCategory	-- copy of the most recent lexeme category, if one exists
 		
-		local position	-- index of the next character in the program
-		local state		-- the current state in the state machine
-		local char		-- the current character
-		local lexString	-- the current lexeme string
-		local category	-- the lexeme category as set when we reach the DONE state
-		local handler	-- state-handler table
-		local quoteType -- preserves the type of quote char that initializes a string literal
-		
-		-- States
-		local DONE = 0
-		local START = 1
+		-- ***** States *****
+		local DONE   = 0
+		local START  = 1
 		local LETTER = 2
 		local NUMERIC = 3
-		local STRING = 4
-		local OPERATOR = 5
+		local EXPONENTIAL = 4
+		local STRING = 5
+		local NUMOPERATOR = 6
+		local BOOLOPERATOR = 7
 		
-		-- Character-related utility functions
-		-- currentChar
-		-- Returns the current character at index position
-		local function currentChar()
+		-- ***** Character-Related Utility Functions *****
+		-- currChar
+		-- Return the current character, at index position in program. Return
+		-- value is a single-character string, or the empty string if position is
+		-- past the end.
+		local function currChar()
 			return program:sub(position, position)
 		end
-		
 		-- nextChar
-		-- Returns the character at position+1
+		-- Return the next character, at index position+1 in program. Return
+		-- value is a single-character string, or the empty string if position+1
+		-- is past the end.
 		local function nextChar()
 			return program:sub(position+1, position+1)
 		end
-		
 		-- nextNextChar
-		-- Returns the character at position+2
+		-- Return the next next character, at index position+2 in program. Return
+		-- value is a single-character string, or the empty string if position+2
+		-- is past the end.
 		local function nextNextChar()
 			return program:sub(position+2, position+2)
 		end
-		
-		-- advanceChar
-		-- Advances position to next character
-		local function advanceChar()
+		-- drop1
+		-- Move position to the next character.
+		local function drop1()
 			position = position+1
 		end
-		
-		-- addChar
-		-- Advances position to next character after adding current character to the lexeme
-		local function addChar()
-			lexString = lexString .. currentChar()
-			nextChar()
+		-- add1
+		-- Add the current character to the lexeme, moving position to the next
+		-- character.
+		local function add1()
+			lexString = lexString .. currChar()
+			drop1()
 		end
-		
 		-- skipWhitespace
-		-- Skips over whitespace characters and comments, moving the position index to the beginning
-		-- of the next potential lexeme
+		-- Skip whitespace and comments, moving position to the beginning of
+		-- the next lexeme, or to program:len()+1.
 		local function skipWhitespace()
-			while true do
-				-- skip whitespace characters
-				while isWhitespace(currentChar()) do
-					advanceCharChar()
+			while true do      -- In whitespace
+				while isWhitespace(currChar()) do
+					drop1()
 				end
-				
-				-- check for comment start
-				if currentChar() ~= "#" then
+				if currChar() ~= "#" then -- Comment?
 					break
 				end
-
-				-- in a comment, advance until comment over
-				advanceChar()
-				while true do
-					-- comment terminates
-					if currentChar() == "#" then
-						advanceChar()
+				drop1()
+				while true do  -- In comment
+					if currChar() == "\n" then
+						drop1()
 						break
-					-- end of input
-					elseif currentChar() == "" then
-						return
+					elseif currChar() == "" then  -- End of input?
+					   return
 					end
-					advanceChar()
+					drop1()
 				end
 			end
 		end
-		
-		
-		-- ******************************************************************
-		-- State Handler Functions
-		-- Functions named according to the state they handle, as handleSTATE
-		-- ******************************************************************
-		local function handleDONE()
-			io.write("ERROR: 'DONE' state should not be handled.\n")
+		-- munchOverride
+		-- Returns true if previous lexeme would override the max munch rule
+		-- based on lexeme spec, false otherwise.
+		local function munchOverride()
+			return previousLexeme == ")" or previousLexeme == "]"
+				or previousLexeme == "true" or previousLexeme == "false"
+				or previousCategory == lexit.ID or previousCategory == lexit.NUMLIT
+		end
+		-- ***** State-Handler Functions *****
+		-- A function with a name like handle_XYZ is the handler function
+		-- for state XYZ
+		local function handle_DONE()
+			io.write("ERROR: 'DONE' state should not be handled\n")
 			assert(0)
 		end
-
-		local function handleSTART()
+		local function handle_START()
 			if isIllegal(char) then
-				addChar()
+				add1()
 				state = DONE
 				category = lexit.MAL
 			elseif isLetter(char) or char == "_" then
-				addChar()
+				add1()
 				state = LETTER
 			elseif isDigit(char) then
-				addChar()
+				add1()
 				state = NUMERIC
-			elseif isQuote(char) then
-				quoteType = char
-				addChar()
+			elseif char == "\'" or char == "\"" then
+				add1()
 				state = STRING
-			elseif isOperator(char) then
-				if char == "+" or char == "-" then
-					if isDigit(nextChar()) then
-						addChar()
-						addChar()
-						state = NUMERIC
-					end
+			elseif char == "+" or char == "-" then
+				if isDigit(nextChar()) and not munchOverride() then
+					add1()
+					state = NUMERIC
 				else
-					addChar()
-					state = OPERATOR
+					add1()
+					state = DONE
+					category = lexit.OP
 				end
+			elseif char == "*" or char == "%" or char == "/"
+				or char == "[" or char == "]" then
+					add1()
+					state = DONE
+					category = lexit.OP
+			elseif char == "!" or char == "=" or char == "<" or char == ">" then
+				add1()
+				state = NUMOPERATOR
+			elseif char == "|" or char == "&" then
+				add1()
+				state = BOOLOPERATOR
 			else
-				addChar()
+				add1()
 				state = DONE
 				category = lexit.PUNCT
 			end
 		end
-			
-		local function handleLETTER()
+		local function handle_LETTER()
 			if isLetter(char) or isDigit(char) or char == "_" then
-				addChar()
+				add1()
 			else
 				state = DONE
 				if isKeyword(lexString) then
@@ -310,26 +284,21 @@ local lexit = {}
 				else
 					category = lexit.ID
 				end
-				
-			end					
+			end
 		end
-		
-		local function handleNUMERIC()
+		local function handle_NUMERIC()
 			if isDigit(char) then
-				addChar()
-			elseif char == "E" or char=="e" then
+				add1()
+			elseif char == "E" or char == "e" then
 				if isDigit(nextChar()) then
-					addChar()
-					addChar()
-				elseif nextChar() == "+" then
-					if isDigit(nextNextChar()) then
-						addChar()
-						addChar()
-						addChar()
-					else
-						state = DONE
-						category = lexit.NUMLIT
-					end
+					add1()
+					add1()
+					state = EXPONENTIAL
+				elseif nextChar() == "+" and isDigit(nextNextChar()) then
+					add1()
+					add1()
+					add1()
+					state = EXPONENTIAL
 				else
 					state = DONE
 					category = lexit.NUMLIT
@@ -337,77 +306,84 @@ local lexit = {}
 			else
 				state = DONE
 				category = lexit.NUMLIT
-			end				
+			end
 		end
-		
-		local function handleSTRING()
-			if char == quoteType then
-				addChar()
+		local function handle_EXPONENTIAL()
+			if isDigit(char) then
+				add1()
+			else
 				state = DONE
-				category = lexit.STRING
-			elseif char == "\n" or char == "" then
+				category = lexit.NUMLIT
+			end
+		end
+		local function handle_STRING()
+			if char == "\n" or char == "" then
+				add1()
 				state = DONE
 				category = lexit.MAL
+			elseif char == string.sub(lexString, 1, 1) then
+				add1()
+				state = DONE
+				category = lexit.STRLIT
 			else
-				addChar()
+				add1()
+			end
+		end
+		local function handle_NUMOPERATOR()
+			if char == "=" then
+				add1()
+			end
+			state = DONE
+			category = lexit.OP
+		end
+		local function handle_BOOLOPERATOR()
+			if char == lexString then
+				add1()
+				state = DONE
+				category = lexit.OP
+			else
+				state = DONE
+				category = lexit.PUNCT
 			end
 		end
 		
-		local function handleOPERATOR()
-			local tempLexString = lexString .. char
-			
-			if isOperator(tempLexString) then
-				addChar()
-				state = DONE
-				category = lexit.OPERATOR
-			else
-				state = DONE
-				category = lexit.OPERATOR
-			end
-		end
-		
-		-- ********************************
-		-- Table of State Handler Functions
-		-- ********************************
+		-- ***** Table of State-Handler Functions *****
 		handlers = {
-			[DONE] = handleDONE,
-			[START] = handleSTART,
-			[LETTER] = handleLETTER,
-			[NUMERIC] = handleNUMERIC,
-			[STRING] = handleSTRING,
-			[OPERATOR] = handleOPERATOR,
+			[DONE]=handle_DONE,
+			[START]=handle_START,
+			[LETTER]=handle_LETTER,
+			[NUMERIC]=handle_NUMERIC,
+			[EXPONENTIAL]=handle_EXPONENTIAL,
+			[STRING]=handle_STRING,
+			[NUMOPERATOR]=handle_NUMOPERATOR,
+			[BOOLOPERATOR]=handle_BOOLOPERATOR,
 		}
-		
-		
-		-- *****************************************************
-		-- Iterator Functions
+		-- ***** Iterator Function *****
 		-- getLexeme
-		-- Gets called each pass through the for-in loops.
-		-- Returns a pair: lexeme (string) and category (int) or
-		-- returns nil, nil if no futher lexemes
-		-- *****************************************************
+		-- Called each time through the for-in loop.
+		-- Returns a pair: lexeme-string (string) and category (int), or
+		-- nil, nil if no more lexemes.
 		local function getLexeme(dummy1, dummy2)
 			if position > program:len() then
 				return nil, nil
 			end
-			
 			lexString = ""
-			state = "START"
+			state = START
 			while state ~= DONE do
-				char = currentChar()
+				char = currChar()
 				handlers[state]()
 			end
-			
 			skipWhitespace()
-			
+			previousLexeme, previousCategory = lexString, category
 			return lexString, category
 		end
-		
-		-- ***********
-		-- Body of lex
-		-- ***********
+		-- ***** Body of Function lex *****
+		-- Initialize & return the iterator function
 		position = 1
 		skipWhitespace()
 		return getLexeme, nil, nil
 	end
+	-- *********************************************************************
+	-- Module Table Return
+	-- *********************************************************************
 return lexit
